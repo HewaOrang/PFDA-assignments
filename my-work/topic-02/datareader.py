72355f550a24ebe330627605cfc3ec3352d8056a
HewaOrang <hewaorang@gmail.com>
2025-10-06 17:16:57 +0000

    topic-02

---
+++ my-work/topic-02/datareader.py
@@ -0,0 +1,14 @@
+# This programe reads data from a CSV file and prints each line.
+# Author: Hewa Orang
+
+import csv
+
+FILENAME = 'data.csv'
+DATADIR = "my-work/topic-02/data"
+
+with open(DATADIR + FILENAME, "rt") as fp:
+    reader = csv.reader(fp, delimiter=",")
+    for line in reader:
+        print (line)
+
+        
